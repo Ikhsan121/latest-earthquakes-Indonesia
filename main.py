580354b97640f2124bc5500bc4cf3e1f6932b6af
M Ikhsan Arif <108288459+Ikhsan121@users.noreply.github.com>
2024-06-15 01:25:18 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Aplikasi deteksi gempa terkini
+"""
+from gempaterkini import data_extraction, show_data, create_csv
+
+if __name__ == "__main__":
+    print('Aplikasi Utama')
+    create_csv()
